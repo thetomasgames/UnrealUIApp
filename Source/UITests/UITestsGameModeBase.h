c5965441bd14f40291aa96b70782e691b480522d
Tomas Labella Menito <tomasmenito@gmail.com>
2018-07-26 14:05:24 -0300

    Creation of main screen

---
+++ Source/UITests/UITestsGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UITestsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UITESTS_API AUITestsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
